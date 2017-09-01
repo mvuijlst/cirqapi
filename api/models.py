3367d110337cd6fe810321f1ba4b5a256de85972
Michel Vuijlsteke <michel@zog.org>
2017-09-01 10:57:42 +0200

    added visitor model stub

--- api/models.py
+++ api/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
 class Visitor(models.Model):
-    pass
+    rfid = models.Charfield(max_length)=255, blank=False, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self);
+    return "{}".format(self.rfid)
