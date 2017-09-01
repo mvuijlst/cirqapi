a22f0b9dd390207c42601087541c0f97f9ae38e3
Michel Vuijlsteke <michel@zog.org>
2017-09-01 13:14:12 +0200

    addeed serializer, test for api

---
+++ api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Visitor
+
+class VisitorSerializer(serializers.ModelSerializer):
+    """Serializer to map the Model instance into JSON format."""
+
+    class Meta:
+        """Meta class to map serializer's fields with the model fields."""
+        model = Visitor
+        fields = ('rfid')
+        read_only_fields = ('date_created', 'date_modified')
--- api/tests.py
+++ api/tests.py
@@ -1,4 +1,7 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.core.urlresolvers import reverse
 from .models import Visitor
 
 class ModelTestCase(TestCase):
@@ -8,10 +11,26 @@ class ModelTestCase(TestCase):
         """Define the test client and other test variables"""
         self.visitor_rfid = "9464d70e"
         self.visitor = Visitor(rfid=self.visitor_rfid)
-    
+
     def test_model_can_create_a_visitor(self):
         """Test the visitor model can create a visitor"""
         old_count = Visitor.objects.count()
         self.visitor.save()
         new_count = Visitor.objects.count()
         self.assertNotEqual(old_count, new_count)
+
+class ViewTestCase(TestCase):
+    """Test suite for api views"""
+
+    def setUp(self):
+        """Define the test client and other test variables"""
+        self.client = APIClient()
+        self.visitor_data = {'rfid': '9464d70e'}
+        self.response = self.client.post(
+            reverse('create'),
+            self.visitor_data,
+            format="json")
+
+    def test_api_can_create_a_visitor(self):
+        """Test the api can create a visitor"""
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
