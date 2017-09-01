a7887152fedf283edff518ad026ec1b20dca79fc
Michel Vuijlsteke <michel@zog.org>
2017-09-01 10:53:53 +0200

    initial test

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Visitor
 
-# Create your tests here.
+class ModelTestCase(TestCase):
+    """This class defines the test suite for the visitor model"""
+
+    def setUp(self):
+        """Define the test client and other test variables"""
+        self.visitor_rfid = "9464d70e"
+        self.visitor = Visitor(rfid=self.visitor_rfid)
+    
+    def test_model_can_create_a_visitor(self):
+        """Test the visitor model can create a visitor"""
+        old_count = Visitor.objects.count()
+        self.visitor.save()
+        new_count = Visitor.objects.count()
+        self.assertNotEqual(old_count, new_count)
