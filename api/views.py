04aca00c97bae4c6f20a327d802a05156dec763c
Michel Vuijlsteke <michel@zog.org>
2017-09-01 13:25:15 +0200

    api stub

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from .serializers import VisitorSerializer
+from .models import Visitor
+
+class CreateView(generics.ListCreateAPIView):
+    """This class defines the create behavior of our rest api"""
+    queryset = Visitors.objects.all()
+    serializer_class = VisitorSerializer
+
+    def perform_create(self, serializer):
+        """Save the post data when creating a new visitor"""
+        serializer.save()
